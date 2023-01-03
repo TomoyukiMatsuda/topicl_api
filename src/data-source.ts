4e113a465444b20ab731694cefb9455c3cba0983
松田智之 <matsudatomoyuki@MatsudaTomoyukinoMacBook-Air.local>
2023-01-03 13:55:32 +0900

    db 接続

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,12 +6,12 @@ export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
+    username: "topicl",
+    password: '',
+    database: 'test',
+    synchronize: false,
+    logging: true,
     entities: [User],
     migrations: [],
     subscribers: [],
-})
+});
